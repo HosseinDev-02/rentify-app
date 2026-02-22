7c7cf1bfefb20e67cd09fb6eeaa00914ce5a3574
HosseinDev-02 <rostamidev2002@gmail.com>
2026-02-22 17:57:40 +0330

    customizing days of calendar

--- src/components/ui/auth/MyDatePicker.js
+++ src/components/ui/auth/MyDatePicker.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Calendar } from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
+import DateObject from "react-date-object";
 
 const weekDays = ["ش", "ی", "د", "س", "چ", "پ", "ج"];
 
@@ -14,6 +15,7 @@ export default function MyDatePicker() {
                 weekDays={weekDays}
                 calendar={persian}
                 locale={persian_fa}
+               showOtherDays
             />
         </div>
     );
